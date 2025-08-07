2be247c0cf129751b20b8a83a8e9d7d116b8fa99
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-08-07 22:14:08 +0530

    this is the fist look

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,14 @@
+import Navbar from "@/components/Navbar";
+import { ClerkProvider } from "@clerk/clerk-expo";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <ClerkProvider>
+        <Navbar />
+        <Stack />
+      </ClerkProvider>
+    </>
+  );
 }
