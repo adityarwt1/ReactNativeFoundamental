4a32c64ab16e71c083e3349dbc3a84d250daa411
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-08-08 12:06:01 +0530

    hard codding time

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 
 export default function Navbar() {
+  // const initialfetch = async () => {
+  //   const respone = await fetch("https://securopass.vercel.app/api/hello", {
+  //     method: "POST",
+  //   });
+  //   const data = await respone.json();
+  //   console.log(data);
+  // };
+  // useEffect(() => {
+  //   initialfetch();
+  // }, []);
   return (
     <View>
       <Text>This is navbar</Text>
