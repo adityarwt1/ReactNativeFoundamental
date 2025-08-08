7ed97d30eca3dff3f6967da9f1a28a81b218d400
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-08-08 13:41:37 +0530

    signin page

--- app/(tabs)/signin.tsx
+++ app/(tabs)/signin.tsx
@@ -4,7 +4,11 @@ import { StyleSheet, Text, View } from "react-native";
 const login = () => {
   return (
     <View style={styles.mainview}>
-      <Text>login</Text>
+      <View style={styles.signinContent}>
+        <Text>Heloo</Text>
+        <Text>Heloo</Text>
+        <Text>Heloo</Text>
+      </View>
     </View>
   );
 };
@@ -16,5 +20,24 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  signinContent: {
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+    height: 100,
+    borderRadius: 10,
+    borderColor: "#f",
+    padding: 10,
+    // Shadow properties for iOS
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.05,
+    shadowRadius: 2,
+    // Elevation for Android
+    elevation: 1,
   },
 });
