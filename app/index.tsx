d6fc848ab413dd2774017f3a042cf0edd9cf8cc4
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-08-08 21:05:01 +0530

    button use

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,34 @@
+import { Image } from "expo-image";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 const index = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Index Page</Text>
+      <Image
+        style={{ width: 100, height: 200 }}
+        source={{
+          uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFYqoKTu_o3Zns2yExbst2Co84Gpc2Q1RJbA&s",
+        }}
+      />
+
+      <TouchableOpacity onPress={() => alert("hello")}>
+        <Text>Button</Text>
+      </TouchableOpacity>
+
+      <Pressable
+        style={{ padding: 10, backgroundColor: "yellow" }}
+        onPress={() => alert("this from the pressable componenct")}
+      >
+        <Text>This is the pressable button </Text>
+      </Pressable>
     </View>
   );
 };
